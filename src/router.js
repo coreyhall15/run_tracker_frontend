6c72dcb0ce3845663d8a3337d3f0ad95e9827e01
Corey Hall <cohall15@gmail.com>
2023-02-21 20:00:14 -0500

    first commit

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import { createBrowserRouter, createRoutesFormElements, Route} from "react-router-dom"
+import App from "./App"
+import Index from "./pages/Index"
+import Show from "./pages/Show"
+
+const router = createBrowserRouter(createRoutesFormElements(
+    <Route path="/" element={<App/>}>
+        <Route path="" element={<Index/>}/>
+        <Route path="post/:id" element={<Show/>}/>
+        <Route path="create"/>
+        <Route path="update/:id"/>
+        <Route path="delete/:id"/>
+    </Route>
+
+))
+
+export default router
